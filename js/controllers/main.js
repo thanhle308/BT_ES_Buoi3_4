b4d68267baaa2d5ec16088142109fc268941c94b
Thanh <lechithanh.devops@gmail.com>
2022-12-15 09:32:49 +0700

    update

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -2,7 +2,7 @@ import TodoService from "../services/todoServices.js";
 import Todo from "../models/Todo.js";
 
 const todoSer = new TodoService();
-let todoArrList = []; 
+let todoArrList = [];
 
 
 const getTodoListMain = () => {
@@ -38,17 +38,22 @@ const showTodoList = (todoArr) => {
 
 const addTodoMain = () => {
     let name = document.getElementById("newTask").value;
-    let status = "false";
-    let newTodo = new Todo(name, status);
+    if (name.trim() !== "") {
+        let status = "false";
+        let newTodo = new Todo(name, status);
 
-    todoSer.addTodo(newTodo)
-        .then((result) => {
-            document.getElementById("newTask").value = "";
-            getTodoListMain();
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+        todoSer.addTodo(newTodo)
+            .then((result) => {
+                document.getElementById("newTask").value = "";
+                getTodoListMain();
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        document.getElementById("notion").innerHTML = "";
+    } else {
+        document.getElementById("notion").innerHTML = "Name Task illegal";
+    }
 
 }
 window.addTodoMain = addTodoMain;
@@ -84,21 +89,21 @@ window.updateTodoMain = updateTodoMain;
 const sortTodo = (type) => {
     if (type == "down") {
         // Hàm sắp xếp sort a-z
-        todoArrList.sort((todo1,todo2)=>{
-            let a= todo1.name.toLowerCase();
-            let b= todo2.name.toLowerCase();
-            return a===b ? 0 : a>b ? 1 : -1;
+        todoArrList.sort((todo1, todo2) => {
+            let a = todo1.name.toLowerCase();
+            let b = todo2.name.toLowerCase();
+            return a === b ? 0 : a > b ? 1 : -1;
         });
-        console.log("done",todoArrList);
-    showTodoList(todoArrList);
+        console.log("done", todoArrList);
+        showTodoList(todoArrList);
     } else if (type = "up") {
         // Sort ngược lại  z-a thì đổi a>b thành a<b
-        todoArrList.sort((todo1,todo2)=>{
-            let a= todo1.name.toLowerCase();
-            let b= todo2.name.toLowerCase();
-            return a===b ? 0 : a<b ? 1 : -1;
+        todoArrList.sort((todo1, todo2) => {
+            let a = todo1.name.toLowerCase();
+            let b = todo2.name.toLowerCase();
+            return a === b ? 0 : a < b ? 1 : -1;
         });
-    showTodoList(todoArrList);
+        showTodoList(todoArrList);
     }
 }
 window.sortTodo = sortTodo;
